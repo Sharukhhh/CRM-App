73f78509101ecb5d287213a0150b1b848355d0e3
Sharuk <msharuk.b10@gmail.com>
2024-04-11 16:29:12 +0530

    some changes

--- back-end/app.js
+++ back-end/app.js
@@ -6,8 +6,11 @@ import connectDB from './database/connect.js'
 
 const app = express();
 
+const corsOptions = {
+    origin: 'https://crm-app-navy.vercel.app/',
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('common'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
